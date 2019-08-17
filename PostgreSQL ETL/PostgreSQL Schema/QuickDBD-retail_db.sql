aa843f92da08f8283c461b8c641fbb38fbf59421
ZGrinacoff <zgrinacoff@gmail.com>
2019-08-17 11:52:38 -0700

    Moved folder

---
+++ PostgreSQL ETL/PostgreSQL Schema/QuickDBD-retail_db.sql
@@ -0,0 +1,49 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/rj1NjW
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+-- CREATE TABLE stores (
+--     Store VARCHAR NOT NULL,
+--     Type VARCHAR NOT NULL,
+--     Size INTEGER NOT NULL,
+--     CONSTRAINT pk_stores PRIMARY KEY (
+--         Store
+--      )
+-- );
+
+-- CREATE TABLE sales (
+--     Store VARCHAR NOT NULL,
+--     Dept VARCHAR NOT NULL,
+--     Date DATE NOT NULL,
+--     Weekly_Sales FLOAT NOT NULL,
+--     Is_Holiday BOOLEAN NOT NULL
+-- );
+
+-- CREATE TABLE features (
+--     Store VARCHAR NOT NULL,
+--     Date DATE NOT NULL,
+--     Temperature FLOAT NOT NULL,
+--     Fuel_Price FLOAT NOT NULL,
+--     Mark_Down_1 FLOAT NOT NULL,
+--     Mark_Down_2 FLOAT NOT NULL,
+--     Mark_Down_3 FLOAT NOT NULL,
+--     Mark_Down_4 FLOAT NOT NULL,
+--     Mark_Down_5 FLOAT NOT NULL,
+--     CPI FLOAT NOT NULL,
+--     Unemployment FLOAT NOT NULL,
+--     Is_Holiday BOOLEAN NOT NULL
+-- );
+
+-- CREATE TABLE holidays (
+--     Holiday VARCHAR NOT NULL,
+--     Date DATE NOT NULL
+-- );
+
+-- SELECT * FROM features;
+
+-- SELECT * FROM holidays;
+
+-- SELECT * FROM stores;
+
+-- SELECT * FROM sales;
